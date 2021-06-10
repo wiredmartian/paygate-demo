cef57602163e171174c09ac8734616b3ca94d0ad
wiredmartian <meggatym@yahoo.com>
2021-06-10 19:59:13 +0200

    Paygate now needs both return and notify urls to be remote

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,8 +2,8 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {Product} from '../models/product.model';
 import {environment} from '../../environments/environment.prod';
-import * as moment from "moment";
-import * as CryptoJS from "crypto-js";
+import * as moment from 'moment';
+import * as CryptoJS from 'crypto-js';
 
 @Injectable({
   providedIn: 'root'
@@ -68,28 +68,28 @@ export class ProductService {
   initiatePayment(product: Product) {
     /**
      * PAYGATE will only consider the NOTIFY_URL if its a remote and valid POST URL
-     * TRANSACTION_DATE format '2020-06-12 09:30:10'
-     * */
+     * 12-06-2021 The RETURN_URL now also needs to be a valid remote url :(
+     */
     const payment = {
-      'PAYGATE_ID': environment.PAYGATEID,
-      'REFERENCE': `#${product.productCode}`,
-      'AMOUNT': (product.price * 100).toString(),
-      'CURRENCY': 'ZAR',
-      'RETURN_URL': 'http://localhost:4000/payment-result',
-      'TRANSACTION_DATE': moment().format('YYYY MM DD hh:mm:ss'),
-      'LOCALE': 'en-za',
-      'COUNTRY': 'ZAF',
-      'EMAIL': 'solomzi.jikani@gmail.com',
-      'NOTIFY_URL': 'http://localhost:4000/payment-details',
+      PAYGATE_ID: environment.PAYGATEID,
+      REFERENCE: `#${product.productCode}`,
+      AMOUNT: (product.price * 100).toString(),
+      CURRENCY: 'ZAR',
+      RETURN_URL: 'https://your-website.com/payment/result',
+      TRANSACTION_DATE: moment().format('YYYY MM DD hh:mm:ss'),
+      LOCALE: 'en-za',
+      COUNTRY: 'ZAF',
+      EMAIL: 'martian@test.com',
+      NOTIFY_URL: 'https://your-website.com/payment/complete',
     };
     // @ts-ignore
-    const checksumFormat = `${payment.PAYGATE_ID}${payment.REFERENCE}${payment.AMOUNT}${payment.CURRENCY}${payment.RETURN_URL}`+
+    const checksumFormat = `${payment.PAYGATE_ID}${payment.REFERENCE}${payment.AMOUNT}${payment.CURRENCY}${payment.RETURN_URL}` +
       `${payment.TRANSACTION_DATE}${payment.LOCALE}${payment.COUNTRY}${payment.EMAIL}${payment.NOTIFY_URL}${environment.PAYGATEKEY}`;
     (payment as any).CHECKSUM = CryptoJS.MD5(checksumFormat, environment.PAYGATEKEY).toString();
     // @ts-ignore
-    let body = new HttpParams({fromObject: payment });
+    const body = new HttpParams({fromObject: payment });
     return this.http.post('https://secure.paygate.co.za/payweb3/initiate.trans', body, {
-      responseType: "text"
+      responseType: 'text'
     });
   }
 }
